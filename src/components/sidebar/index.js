342e8740e3ee1c06292adc99314de50925d060dc
flie2u <88200564+flie2u@users.noreply.github.com>
2023-08-12 14:58:52 +0700

    more development

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,14 +1,21 @@
 import './index.scss'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import logoC from '../../assets/images/logo-civic.jpg'
 import logoSub from '../../assets/images/logo-civic.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to ='/'>
+    <div className="nav-bar">
+        <Link className="logo" to ="/">
             <img src={logoC} alt="logo" />
-            <img src={logoSub} alt="logo_sub" />
-        </Link>
+            <img className="sub-logo" src={logoSub} alt="logo_sub" />
+        </Link> 
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+        </nav>
     </div>
 )
 
